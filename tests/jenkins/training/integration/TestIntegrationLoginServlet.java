cd711c9818ca764a5fe7d0a06107180a13ab15ff
Krzysztof Kałużny | hybris <krzysztof.kaluzny@hybris.com>
2015-03-02 17:45:29 +0100

    initial

---
+++ tests/jenkins/training/integration/TestIntegrationLoginServlet.java
@@ -0,0 +1,35 @@
+package jenkins.training.integration;
+
+import org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import org.junit.Test;
+
+
+public class TestIntegrationLoginServlet {
+
+	@Test
+	public void test1() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test2() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test3() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test4() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test5() {	
+		assertEquals(73, 73);
+	}
+}
---
+++ tests/jenkins/training/unit/TestLoginServlet.java
@@ -0,0 +1,56 @@
+package jenkins.training.unit;
+
+import org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import org.junit.Test;
+
+
+public class TestLoginServlet {
+
+	@Test
+	public void test1() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test2() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test3() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test4() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test5() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test6() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test7() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test8() {	
+		assertEquals(73, 73);
+	}
+	
+	@Test
+	public void test9() {	
+		assertEquals(73, 73);
+	}
+	
+}
