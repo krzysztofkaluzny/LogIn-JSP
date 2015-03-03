97722888a897075adc04e4e7e84844ab93dff3f6
Your Name <you@example.com>
2015-03-03 22:21:52 +0100

    deliberate failing the test

--- tests/jenkins/training/integration/TestIntegrationLoginServlet.java
+++ tests/jenkins/training/integration/TestIntegrationLoginServlet.java
@@ -9,27 +9,27 @@ import org.junit.Test;
 public class TestIntegrationLoginServlet {
 
 	@Test
-	public void test1() {	
+	public void test1() {
+		assertEquals(73, 743);
+	}
+
+	@Test
+	public void test2() {
 		assertEquals(73, 73);
 	}
-	
+
 	@Test
-	public void test2() {	
+	public void test3() {
 		assertEquals(73, 73);
 	}
-	
+
 	@Test
-	public void test3() {	
+	public void test4() {
 		assertEquals(73, 73);
 	}
-	
+
 	@Test
-	public void test4() {	
-		assertEquals(73, 73);
-	}
-	
-	@Test
-	public void test5() {	
+	public void test5() {
 		assertEquals(73, 73);
 	}
 }
