b8891bda8dd9909aa0f1a13bdf60105437e1ea0f
John Doe <johndoe@example.com>
2015-03-03 14:14:33 +0100

    fixing deliberate erro

--- src/jenkins/training/LoginServlet.java
+++ src/jenkins/training/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-test
+
 
 /**
  * Servlet implementation class LoginServlet
