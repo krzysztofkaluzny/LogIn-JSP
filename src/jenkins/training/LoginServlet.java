4a097a3a03cc37f6d52c0c1bf6da05a53cc9c0c4
John Doe <johndoe@example.com>
2015-03-03 14:13:03 +0100

    deliberate error

--- src/jenkins/training/LoginServlet.java
+++ src/jenkins/training/LoginServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+test
+
 /**
  * Servlet implementation class LoginServlet
  */
